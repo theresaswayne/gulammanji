e5336d3337d153cc455f37f13f1853f059666b8a
Theresa Swayne <theresaswayne@users.noreply.github.com>
2026-02-01 23:51:06 -0500

    vimentin analysis and renaming

--- count_cells_over_median.R
+++ count_cells_over_median.R
@@ -1,7 +1,14 @@
-# count_positive_cells_CP.R
+# count_cells_over_median.R
 # R script to use mean intensity data from CellProfiler object results files
 # to count cells exceeding a threshold intensity value defined as the median of the control group
 # and summarize by treatment and sample
+
+# Theresa Swayne, Columbia University, 2025-26
+# -------- Suggested text for acknowledgement -----------
+#   "These studies used the Confocal and Specialized Microscopy Shared Resource 
+#   of the Herbert Irving Comprehensive Cancer Center at Columbia University, 
+#   funded in part through the NIH/NCI Cancer Center Support Grant P30CA013696."
+
 # To use: Run the script.
 # Will be prompted for a file.
 
@@ -19,16 +26,38 @@ objectFile <- file.choose()
 objectData <- read_csv(objectFile,
                     locale = locale())
 
+# template provided for calculating vimentin positivity -- but this measurement is not appropriate if cells are detected using vimentin
+
 # determine threshold for positivity by median of control group
+# medians <- objectData %>% 
+# group_by(Metadata_Treatment) %>% 
+# summarise(nCells = n(),
+#           medianSMA = median(Intensity_MeanIntensity_SMA),
+#           medianPDGFR = median(Intensity_MeanIntensity_PDGFRalpha),
+#           medianVimentin = median(Intensity_MeanIntensity_Vimentin))
+
 medians <- objectData %>% 
   group_by(Metadata_Treatment) %>% 
   summarise(nCells = n(),
             medianSMA = median(Intensity_MeanIntensity_SMA),
             medianPDGFR = median(Intensity_MeanIntensity_PDGFRalpha))
+
+# get the control group median from the first row of the summary
 threshSMA <- medians$medianSMA[1]
 threshPDGFR <- medians$medianPDGFR[1]
+#threshVim <- medians$medianVimentin[1]
             
 # ---- Count positive cells within each sample and treatment ---
+# counts <- objectData %>% 
+#   group_by(Metadata_Treatment, Metadata_Sample) %>% 
+#   summarise(nCells = n(),
+#             PositiveSMA = sum(Intensity_MeanIntensity_SMA > threshSMA),
+#             PercentPositiveSMA = PositiveSMA/nCells,
+#             PositivePDGFR = sum(Intensity_MeanIntensity_PDGFRalpha > threshPDGFR),
+#             PercentPositivePDGFR = PositivePDGFR/nCells,
+#             PositiveVimentin = sum(Intensity_MeanIntensity_Vimentin > threshVim),
+#             PercentPositiveVimentin = PositiveVimentin/nCells)
+
 counts <- objectData %>% 
   group_by(Metadata_Treatment, Metadata_Sample) %>% 
   summarise(nCells = n(),
@@ -36,14 +65,34 @@ counts <- objectData %>%
             PercentPositiveSMA = PositiveSMA/nCells,
             PositivePDGFR = sum(Intensity_MeanIntensity_PDGFRalpha > threshPDGFR),
             PercentPositivePDGFR = PositivePDGFR/nCells)
+
+# make the column headers show the threshold used
 SMA_name <- paste0("SMA Mean >",round(threshSMA, digits = 5))
 PDGFR_name <- paste0("PDGFR Mean >", round(threshPDGFR, digits = 5))
+#Vim_name <- paste0("Vimentin Mean >", round(threshVim, digits = 5))
+
+# prepare a file for saving with the informative column headers
 counts_save <- counts
+# counts_save <- counts_save %>% 
+#   rename(!!SMA_name := PositiveSMA) %>%
+#   rename(!!PDGFR_name := PositivePDGFR) %>%
+#   rename(!!Vim_name := PositiveVimentin)
+
 counts_save <- counts_save %>% 
   rename(!!SMA_name := PositiveSMA) %>%
   rename(!!PDGFR_name := PositivePDGFR)
 
 # ---- Count positive cells by treatment group, pooling all samples ---
+
+# pooled_counts <- objectData %>% 
+#   group_by(Metadata_Treatment) %>% 
+#   summarise(nCells = n(),
+#             PositiveSMA = sum(Intensity_MeanIntensity_SMA > threshSMA),
+#             PercentPositiveSMA = PositiveSMA/nCells,
+#             PositivePDGFR = sum(Intensity_MeanIntensity_PDGFRalpha > threshPDGFR),
+#             PercentPositivePDGFR = PositivePDGFR/nCells,
+#             PositiveVimentin = sum(Intensity_MeanIntensity_Vimentin > threshVim),
+#             PercentPositiveVimentin = PositiveVimentin/nCells)
 
 pooled_counts <- objectData %>% 
   group_by(Metadata_Treatment) %>% 
@@ -52,7 +101,13 @@ pooled_counts <- objectData %>%
             PercentPositiveSMA = PositiveSMA/nCells,
             PositivePDGFR = sum(Intensity_MeanIntensity_PDGFRalpha > threshPDGFR),
             PercentPositivePDGFR = PositivePDGFR/nCells)
+            
 pooled_counts_save <- pooled_counts
+# pooled_counts_save <- pooled_counts_save %>% 
+#   rename(!!SMA_name := PositiveSMA) %>%
+#   rename(!!PDGFR_name := PositivePDGFR) %>%
+#   rename(!!Vim_name := PositiveVimentin)
+
 pooled_counts_save <- pooled_counts_save %>% 
   rename(!!SMA_name := PositiveSMA) %>%
   rename(!!PDGFR_name := PositivePDGFR)
@@ -89,10 +144,29 @@ p_countPDGFR <- ggplot(counts,
 outputPlotPDGFR = paste(objectName, "PDGFR count plot.pdf")
 ggsave(file.path(parentDir, outputPlotPDGFR), width=20, height = 14)
 
+# template for plotting vimentin positivity -- but this measurement is not appropriate if cells are detected using vimentin
+# 
+# p_countVim <- ggplot(counts,
+#                        aes(Metadata_Treatment, PercentPositiveVimentin)) + 
+#   geom_col() +
+#   facet_wrap(~Metadata_Sample)+
+#   theme(text=element_text(size=20))
+# 
+# outputPlotVim = paste(objectName, "Vimentin count plot.pdf")
+# ggsave(file.path(parentDir, outputPlotVim), width=20, height = 14)
+
 # to get both on the same plot we have to reshape the data
-pct_pos <- counts %>% select(-c(PositiveSMA, PositivePDGFR)) %>% rename(SMA = PercentPositiveSMA, PDGFR = PercentPositivePDGFR)
+# pct_pos <- counts %>% 
+#   select(-c(PositiveSMA, PositivePDGFR, PositiveVimentin)) %>%
+#   rename(SMA = PercentPositiveSMA, PDGFR = PercentPositivePDGFR, Vim = PercentPositiveVimentin)
+
+pct_pos <- counts %>% 
+  select(-c(PositiveSMA, PositivePDGFR)) %>%
+  rename(SMA = PercentPositiveSMA, PDGFR = PercentPositivePDGFR)
+
 pct_pos_pivot <- pct_pos %>%
   pivot_longer(-c(Metadata_Sample,Metadata_Treatment, nCells), names_to="Marker", values_to="PercentPositive")
+
 # p_counts <- ggplot(pct_pos_pivot,
 #                        aes(x = Metadata_Treatment, y = PercentPositive, fill = Marker)) + 
 #   geom_col(position = "dodge") +
@@ -128,3 +202,13 @@ p_pooledPDGFR <- ggplot(pooled_counts,
 
 outputPooledPDGFR = paste(objectName, "PDGFR pooled count plot.pdf")
 ggsave(file.path(parentDir, outputPooledPDGFR), width=20, height = 14)
+
+# template for plotting vimentin positivity -- but this measurement is not appropriate if cells are detected using vimentin
+# 
+# p_pooledVim <- ggplot(pooled_counts,
+#                         aes(Metadata_Treatment, PercentPositiveVim)) + 
+#   geom_col()+
+#   theme(text=element_text(size=20))
+# 
+# outputPooledVim = paste(objectName, "Vimentin pooled count plot.pdf")
+# ggsave(file.path(parentDir, outputPooledVim), width=20, height = 14)
--- summarize_plot_cellprofiler_data.R
+++ summarize_plot_cellprofiler_data.R
@@ -1,6 +1,13 @@
-# summarize_CP_tables_SD_plot.R
+# summarize_plot_cellprofiler_data.R
 # R script to select mean intensity data from CellProfiler object results files
 # and summarize by treatment and sample
+
+# Theresa Swayne, Columbia University, 2025-26
+# -------- Suggested text for acknowledgement -----------
+#   "These studies used the Confocal and Specialized Microscopy Shared Resource 
+#   of the Herbert Irving Comprehensive Cancer Center at Columbia University, 
+#   funded in part through the NIH/NCI Cancer Center Support Grant P30CA013696."
+
 # To use: Run the script.
 # Will be prompted for a file
 
@@ -8,7 +15,6 @@
 require(tidyverse)
 require(readr)
 require(stringr)
-
 
 # ---- Prompt for an object file ----
 # no message will be displayed
@@ -18,14 +24,15 @@ objectFile <- file.choose()
 objectData <- read_csv(objectFile,
                     locale = locale())
 
-
 mean_intens <- objectData %>% 
   group_by(Metadata_Treatment, Metadata_Sample) %>% 
   summarise(nCells = n(),
             MeanSMA = mean(Intensity_MeanIntensity_SMA),
             MeanSMA_SD = sd(Intensity_MeanIntensity_SMA),
             MeanPDGFR = mean(Intensity_MeanIntensity_PDGFRalpha),
-            MeanPDGFR_SD = sd(Intensity_MeanIntensity_PDGFRalpha))
+            MeanPDGFR_SD = sd(Intensity_MeanIntensity_PDGFRalpha),
+            MeanVim = mean(Intensity_MeanIntensity_Vimentin),
+            MeanVim_SD = sd(Intensity_MeanIntensity_Vimentin))
 
 
 mean_pooled_intens <- objectData %>% 
@@ -34,7 +41,9 @@ mean_pooled_intens <- objectData %>%
             MeanSMA = mean(Intensity_MeanIntensity_SMA),
             MeanSMA_SD = sd(Intensity_MeanIntensity_SMA),
             MeanPDGFR = mean(Intensity_MeanIntensity_PDGFRalpha),
-            MeanPDGFR_SD = sd(Intensity_MeanIntensity_PDGFRalpha))
+            MeanPDGFR_SD = sd(Intensity_MeanIntensity_PDGFRalpha),
+            MeanVim = mean(Intensity_MeanIntensity_Vimentin),
+            MeanVim_SD = sd(Intensity_MeanIntensity_Vimentin))
 
 # ---- Save new file ----
 objectName <- str_sub(basename(objectFile), 1, -5) # name of the file without higher levels or extension
@@ -70,6 +79,18 @@ outputPlotPDGFR = paste(objectName, "PDGFR mean plot.pdf")
 ggsave(file.path(parentDir, outputPlotPDGFR), width=20, height = 14)
 
 
+p_meanVim <- ggplot(objectData,
+                      aes(x=Metadata_Treatment,
+                          y=Intensity_MeanIntensity_Vimentin)) + 
+  geom_violin(trim=FALSE) +
+  facet_wrap(~Metadata_Sample) +
+  stat_summary(fun.data=mean_sdl,
+               geom="pointrange", color="red")
+
+outputPlotVim = paste(objectName, "Vimentin mean plot.pdf")
+ggsave(file.path(parentDir, outputPlotVim), width=20, height = 14)
+
+
 # ---- Plots of pooled data ----
 p_pooledSMA <- ggplot(objectData,
                     aes(x=Metadata_Treatment,
@@ -92,3 +113,14 @@ p_pooledPDGFR <- ggplot(objectData,
 
 outputPooledPDGFR = paste(objectName, "PDGFR mean pooled plot.pdf")
 ggsave(file.path(parentDir, outputPooledPDGFR), width=20, height = 14)
+
+p_pooledVim <- ggplot(objectData,
+                        aes(x=Metadata_Treatment,
+                            y=Intensity_MeanIntensity_Vimentin)) + 
+  geom_violin(trim=FALSE) +
+  stat_summary(fun.data=mean_sdl,
+               geom="pointrange", color="red")
+
+outputPooledVim = paste(objectName, "Vimentin mean pooled plot.pdf")
+ggsave(file.path(parentDir, outputPooledVim), width=20, height = 14)
+
